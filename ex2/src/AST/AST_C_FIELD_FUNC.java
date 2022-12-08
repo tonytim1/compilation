062a5b214194821cabb1a381b0c96b7926704297
tony-cyera <tonyt@cyera.io>
2022-12-08 17:01:38 +0200

    fix

--- ex2/src/AST/AST_C_FIELD_FUNC.java
+++ ex2/src/AST/AST_C_FIELD_FUNC.java
@@ -4,7 +4,7 @@ public class AST_C_FIELD_FUNC extends AST_C_FIELD {
 
 	public AST_FUNC_DEC funcDec;
 	
-	public AST_C_FIELD(AST_FUNC_DEC funcDec) 
+	public AST_C_FIELD_FUNC(AST_FUNC_DEC funcDec)
 	{
 		/******************************/
 		/* SET A UNIQUE SERIAL NUMBER */
