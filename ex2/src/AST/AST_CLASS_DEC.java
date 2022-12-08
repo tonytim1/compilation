db938bfbaee7ef77058d67ce1027d3ef3c6cffd7
tony-cyera <tonyt@cyera.io>
2022-12-08 16:47:55 +0200

    class dec fix

--- ex2/src/AST/AST_CLASS_DEC.java
+++ ex2/src/AST/AST_CLASS_DEC.java
@@ -6,25 +6,6 @@ public class AST_CLASS_DEC extends AST_Node {
 	public String id2;
 	public AST_C_FIELD_LIST cFieldList;
 
-	public AST_CLASS_DEC(String id1, AST_C_FIELD_LIST cFieldList)
-	{
-		/******************************/
-		/* SET A UNIQUE SERIAL NUMBER */
-		/******************************/
-		SerialNumber = AST_Node_Serial_Number.getFresh();
-		
-		/***************************************/
-		/* PRINT CORRESPONDING DERIVATION RULE */
-		/***************************************/
-		System.out.format("====================== classDec -> CLASS ID( %s ) LBRACE classCont RBRACE", id1);
-
-		/*******************************/
-		/* COPY INPUT DATA MEMBERS ... */
-		/*******************************/
-		this.id1 = id1;
-		this.id2 = null;
-		this.cFieldList = cFieldList;
-	}
 	
 	public AST_CLASS_DEC(String id1, String id2, AST_C_FIELD_LIST cFieldList)
 	{
@@ -36,7 +17,10 @@ public class AST_CLASS_DEC extends AST_Node {
 		/***************************************/
 		/* PRINT CORRESPONDING DERIVATION RULE */
 		/***************************************/
-		System.out.format("====================== classDec -> CLASS ID( %s ) EXTENDS ID( %s ) LBRACE classCont RBRACE", id1, id2);
+		if ( id2 != null)
+		    System.out.format("====================== classDec -> CLASS ID( %s ) EXTENDS ID( %s ) LBRACE classCont RBRACE", id1, id2);
+		else
+		    System.out.format("====================== classDec -> CLASS ID( %s ) LBRACE classCont RBRACE");
 
 		/*******************************/
 		/* COPY INPUT DATA MEMBERS ... */
