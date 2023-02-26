ea534b3e20865f0b7c642f217b03e96e25321901
royddf503 <royddf1@hotmail.com>
2023-02-26 16:32:25 +0200

    Binop IR new classes

---
+++ ex4/src/IR/IRcommand_Binop_EQ_Strings.java
@@ -0,0 +1,28 @@
+/***********/
+/* PACKAGE */
+/***********/
+package IR;
+
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+import TEMP.*;
+import MIPS.*;
+
+public class IRcommand_Binop_EQ_Strings extends IRcommand
+{
+    public TEMP t1;
+    public TEMP t2;
+    public TEMP dst;
+
+    public IRcommand_Binop_EQ_Strings(TEMP dst,TEMP t1,TEMP t2)
+	{
+		this.dst = dst;
+		this.t1 = t1;
+		this.t2 = t2;
+	}
+}
