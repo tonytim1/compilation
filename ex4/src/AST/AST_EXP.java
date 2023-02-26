829f2c14a0176e759dbf5c576393858660667b11
royddf503 <royddf1@hotmail.com>
2023-02-26 18:04:28 +0200

    Implemented IRme

--- ex4/src/AST/AST_EXP.java
+++ ex4/src/AST/AST_EXP.java
@@ -1,6 +1,8 @@
 package AST;
 import TYPES.*;
 import SYMBOL_TABLE.*;
+import IR.*;
+import TEMP.*;
 
 public abstract class AST_EXP extends AST_Node
 {
@@ -13,5 +15,9 @@ public abstract class AST_EXP extends AST_Node
     public TYPE SemantMe() throws SEMANTIC_EXCEPTION
 	{
 		return null;
+	}
+
+	public TEMP IRme() {
+	    return null;
 	}
 }
