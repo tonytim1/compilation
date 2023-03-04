fcc3361511a3314df63deec16b222809428867a1
royddf503 <royddf1@hotmail.com>
2023-03-04 17:34:33 +0200

    IR - Class new class

---
+++ ex4/src/IR/IRcommand_ClassNew.java
@@ -0,0 +1,26 @@
+/***********/
+/* PACKAGE */
+/***********/
+package IR;
+
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+import TEMP.*;
+import MIPS.*;
+
+public class IRcommand_ClassNew extends IRcommand
+{
+	TEMP dst;
+	String name;
+
+	public IRcommand_ClassNew(TEMP dst, String name)
+	{
+		this.dst = dst;
+		this.name = name;
+	}
+}
