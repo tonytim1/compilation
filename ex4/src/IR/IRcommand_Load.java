c12afcd803e2b88babb23bbf94364e95d972c045
agal <agal@vmware.com>
2023-03-12 19:37:19 +0200

    add store & load MIPSME

--- ex4/src/IR/IRcommand_Load.java
+++ ex4/src/IR/IRcommand_Load.java
@@ -2,6 +2,7 @@
 /* PACKAGE */
 /***********/
 package IR;
+
 import TYPES.*;
 import TEMP.*;
 import MIPS.*;
@@ -13,24 +14,48 @@ import REGALLOC.*;
 
 /*******************/
 /* PROJECT IMPORTS */
+
 /*******************/
 
-public class IRcommand_Load extends IRcommand
-{
-	TEMP dst;
-	String var_name;
-	
-	public IRcommand_Load(TEMP dst,String var_name)
-	{
-		this.dst      = dst;
-		this.var_name = var_name;
-	}
-	
-	/***************/
-	/* MIPS me !!! */
-	/***************/
-	public void MIPSme()
-	{
-		MIPSGenerator.getInstance().load(dst,var_name);
-	}
+public class IRcommand_Load extends IRcommand {
+    TEMP dst;
+    String var_name;
+    String scope_type;
+    int index;
+    Boolean is_string;
+
+    public IRcommand_Load(TEMP dst, String var_name, String scope_type, int index, Boolean is_string) {
+        this.dst = dst;
+        this.var_name = var_name;
+        this.scope_type = scope_type;
+        this.index = index;
+        this.is_string = is_string;
+    }
+
+    /***************/
+    /* MIPS me !!! */
+
+    /***************/
+    public void MIPSme() {
+        if (scope_type.equals("global")) {
+            if (is_string)
+                MIPSGenerator.getInstance().load(dst.toString(), var_name);
+            else
+                MIPSGenerator.getInstance().load_label(dst.toString(), var_name);
+        } else if (scope_type.equals("param"))
+            MIPSGenerator.getInstance().lw(dst.toString(), "$fp", (index + 2) * 4);
+        else if (scope_type.equals("local_func"))
+            MIPSGenerator.getInstance().lw(dst.toString(), "$fp", (index + 1) * -4);
+        else if (scope_type.equals("local_class")) {
+            // the first argument of a function is the class pointer
+            MIPSGenerator.getInstance().lw(dst.toString(), "$fp", 8);
+            MIPSGenerator.getInstance().lw(dst.toString(), dst.toString(), (index + 1) * 4);
+        }
+    }
+
+    public TEMP_LIST getLiveTemp(TEMP_LIST input) {
+        TEMP_LIST result = input.clone();
+        result.remove(dst);
+        return result;
+    }
 }
