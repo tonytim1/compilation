00f99c3c7d453961c78fa82bb8be4899b7a5c15e
tonytim <antony@mail.tau.ac.il>
2023-03-15 01:52:20 +0200

    a

--- ex4/src/IR/IRcommand_Binop_NEQ_Strings.java
+++ ex4/src/IR/IRcommand_Binop_NEQ_Strings.java
@@ -13,12 +13,12 @@ package IR;
 import TEMP.*;
 import MIPS.*;
 
-public class IRcommand_Binop_NEQ_Contents extends IRcommand_Binop {
+public class IRcommand_Binop_NEQ_Strings extends IRcommand_Binop {
 	//public TEMP t1;
 	//public TEMP t2;
 	//public TEMP dst;
 
-	public IRcommand_Binop_NEQ_Contents(TEMP dst, TEMP t1, TEMP t2) {
+	public IRcommand_Binop_NEQ_Strings(TEMP dst, TEMP t1, TEMP t2) {
 		this.dst = dst;
 		this.t1 = t1;
 		this.t2 = t2;
@@ -29,7 +29,7 @@ public class IRcommand_Binop_NEQ_Contents extends IRcommand_Binop {
 	/* MIPS me !!! */
 	/***************/
 	public void MIPSme() {
-		System.out.println("IRcommand_Binop_NEQ_Contents" + "- MIPSme");
+		System.out.println("IRcommand_Binop_NEQ_Strings" + "- MIPSme");
 
 		/*******************************/
 		/* [1] Allocate 3 fresh labels */
