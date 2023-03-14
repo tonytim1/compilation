ab6ba9c266aa755e7667398031ca6b80479bf02f
agal <agal@vmware.com>
2023-03-14 20:38:09 +0200

    finish class dec

--- ex4/src/AST/AST_CLASS_DEC.java
+++ ex4/src/AST/AST_CLASS_DEC.java
@@ -3,7 +3,7 @@ import TYPES.*;
 import SYMBOL_TABLE.*;
 import IR.*;
 import TEMP.*;
-import java.util.*;
+
 
 public class AST_CLASS_DEC extends AST_Node {
 
@@ -65,7 +65,7 @@ public class AST_CLASS_DEC extends AST_Node {
 				throw new SEMANTIC_EXCEPTION(lineNumber);
 			}
 			fatherClass = (TYPE_CLASS) father;
-			fatherDataMembers = (TYPE_LIST) fatherClass.data_members;
+			fatherDataMembers = (TYPE_LIST) fatherClass.cFieldList;
 		}
 
 		TYPE_LIST allDataMembersHolder = fatherDataMembers;
@@ -130,13 +130,13 @@ public class AST_CLASS_DEC extends AST_Node {
 			int fieldCnt = 0;
 			int funcCnt = 0;
 
-			for (AST_C_FIELD_LIST it = cFieldList; it != null; it = it.cFieldList) {
-				AST_C_FIELD field = (AST_C_FIELD) (it.cField);
-				if (field instanceof AST_C_FIELD_VAR) { // field
+			for (AST_C_FIELD_LIST it = cFieldList; it != null; it = it.tail) {
+				AST_C_FIELD field = (AST_C_FIELD) (it.head);
+				if (field.typeCField == "varDec") { // field
 					fieldCnt += 1;
 					continue;
 				}
-				if (field instanceof AST_C_FIELD_FUNC) { // func
+				if (field.typeCField == "funcDec") { // func
 					// funcCnt += 1;
 					AST_C_FIELD_FUNC a = (AST_C_FIELD_FUNC) field;
 					AST_FUNC_DEC b = (AST_FUNC_DEC) (a.func);
@@ -144,7 +144,7 @@ public class AST_CLASS_DEC extends AST_Node {
 				}
 
 				AST_C_FIELD_FUNC a = (AST_C_FIELD_FUNC) field;
-				AST_FUNCDEC b = (AST_FUNCDEC) (a.func);
+				AST_FUNC_DEC b = (AST_FUNC_DEC) (a.func);
 
 				ArrayList<String> function = new ArrayList<String>();
 				function.add(b.id);
@@ -159,10 +159,10 @@ public class AST_CLASS_DEC extends AST_Node {
 
 			fieldCnt = 0;
 			ArrayList<String> fieldslist = new ArrayList<>();
-			for (AST_C_FIELD_LIST it = cFieldList; it != null; it = it.cFieldList) {
-				AST_C_FIELD field = (AST_C_FIELD) (it.cField);
+			for (AST_C_FEILD_LIST it = cFieldList; it != null; it = it.tail) {
+				AST_C_FIELD field = (AST_C_FIELD) (it.head);
 
-				if (field instanceof AST_C_FIELD_VAR) { // field
+				if (field.typeCField == "varDec") { // field
 					// fieldCnt += 1;
 					AST_C_FIELD_VAR var = (AST_C_FIELD_VAR) field;
 					AST_VAR_DEC b = (AST_VAR_DEC) (var.vd);
@@ -185,188 +185,189 @@ public class AST_CLASS_DEC extends AST_Node {
 					fields.add(fieldandclass);
 				}
 			}
-			for (AST_C_FIELD_LIST it = cFieldList; it != null; it = it.cFieldList) {
-				if (it.cField instanceof AST_C_FEILD_FUNC) {
-					it.cField.IRme();
+			for (AST_C_FEILD_LIST it = cFieldList; it != null; it = it.tail) {
+				if (it.head.typeCField == "funcDec") {
+					it.head.IRme();
 				}
 			}
 			classSize.put(id, fields.size() * 4 + 4);
 			IR.getInstance().Add_IRcommand(new IRcommand_declareClass(id, funclist, fields));
-			for (AST_C_FIELD_LIST it = cFieldList; it != null; it = it.cFieldList) {
-				if (it.cField instanceof AST_C_FEILD_VAR)
-					it.cField.IRme();
+			for (AST_C_FEILD_LIST it = cFieldList; it != null; it = it.tail) {
+				if (it.head.typeCField == "varDec")
+					it.head.IRme();
 			}
 			classfields.put(id, fieldslist);
-//		} //else {
-//			// Gal stopped - TODO
-//			TYPE_CLASS fatherclass = (TYPE_CLASS) SYMBOL_TABLE.getInstance().find(id2);
-//			boolean f = false;
-//			// ############first part (taking father fields and funcs)#################
-//			ArrayList<ArrayList<ArrayList<String>>> fieldlist = new ArrayList<ArrayList<ArrayList<String>>>();
-//			ArrayList<ArrayList<String>> funclist = new ArrayList<ArrayList<String>>();
-//			while (fatherclass != null) {
-//				for (AST_TYPE_NAME_LIST it = fatherclass.functions; it != null; it = it.tail) {
-//					int n = funclist.size();
-//					for (int i = 0; i < n; i++) {
-//						f = false;
-//						if (((funclist.get(i)).get(0)).equals(it.head.name)) {
-//							ArrayList<String> temp = funclist.get(i);
-//							funclist.remove(i);
-//							funclist.add(0, temp);
-//							break;
-//						}
-//					}
-//					if (f == false) {
-//						ArrayList<String> funcOfFather = new ArrayList<String>();
-//						funcOfFather.add(it.head.name);
-//						funcOfFather.add(fatherclass.name);
-//						funclist.add(0, funcOfFather);
-//					}
-//				}
-//
-//				for (AST_ARG_LIST it = fatherclass.data_members; it != null; it = it.tail) {
-//					f = false;
-//					int n = fieldlist.size();
-//					for (int i = 0; i < n; i++) {
-//						if ((((fieldlist.get(i)).get(0)).get(0)).equals(it.head.id)) {
-//							f = true;
-//							ArrayList<ArrayList<String>> temp = fieldlist.get(i);
-//							fieldlist.remove(i);
-//							fieldlist.add(0, temp);
-//							break;
-//						}
-//					}
-//					if (f == true)
-//						continue;
-//
-//					ArrayList<String> fieldOfFather = new ArrayList<String>();
-//					fieldOfFather.add(it.head.id);
-//					if (it.head.t instanceof AST_TYPE_STRING)
-//						fieldOfFather.add("1");
-//					else
-//						fieldOfFather.add("0");
-//					ArrayList<ArrayList<String>> field = new ArrayList<ArrayList<String>>();
-//					field.add(fieldOfFather);
-//					ArrayList<String> fatherarray = new ArrayList<String>();
-//					fatherarray.add(fatherclass.name);
-//					field.add(fatherarray);
-//					fieldlist.add(0, field);
-//				}
-//				fatherclass = fatherclass.father;
-//			}
-//
-//			// #############################################################3
-//			// #################second part - this class fields and funcs
-//			for (AST_CFEILD_LIST it = data_members; it != null; it = it.tail) {
-//				AST_CFIELD field = (AST_CFIELD) (it.head);
-//				if (field instanceof AST_CFEILD_VARDEC) { // field
-//					f = false;
-//					AST_CFEILD_VARDEC a = (AST_CFEILD_VARDEC) field;
-//					AST_VARDEC b = (AST_VARDEC) (a.vd);
-//					int n = fieldlist.size();
-//					for (int i = 0; i < n; i++) {
-//						if ((((fieldlist.get(i)).get(0)).get(0)).equals(b.id)) {
-//							f = true;
-//							((fieldlist.get(i)).get(1)).set(0, id);
-//							break;
-//						}
-//					}
-//					if (f == false) {
-//						ArrayList<String> fieldOfMe = new ArrayList<String>();
-//						fieldOfMe.add(b.id);
-//						if (b.type instanceof AST_TYPE_STRING)
-//							fieldOfMe.add("1");
-//						else
-//							fieldOfMe.add("0");
-//						ArrayList<ArrayList<String>> field2 = new ArrayList<ArrayList<String>>();
-//						field2.add(fieldOfMe);
-//						ArrayList<String> mearray = new ArrayList<String>();
-//						mearray.add(id);
-//						field2.add(mearray);
-//						fieldlist.add(field2);
-//					}
-//					continue;
-//				}
-//				if (field instanceof AST_CFEILD_FUNCDEC) { // func
-//					f = false;
-//					AST_CFEILD_FUNCDEC a = (AST_CFEILD_FUNCDEC) field;
-//					AST_FUNCDEC b = (AST_FUNCDEC) (a.func);
-//					int n = funclist.size();
-//					for (int i = 0; i < n; i++) {
-//						if (((funclist.get(i)).get(0)).equals(b.id)) {
-//							f = true;
-//							(funclist.get(i)).set(1, id);
-//							break;
-//						}
-//					}
-//					if (f == false) {
-//						ArrayList<String> funcOfMe = new ArrayList<String>();
-//						funcOfMe.add(b.id);
-//						funcOfMe.add(id);
-//						funclist.add(funcOfMe);
-//					}
-//
-//				}
-//			}
-//
-//			// #######################part 3 - offsets ################################
-//			int funcCnt = funclist.size();
-//			int fieldCnt = 0;
-//			Map<String, Integer> funcOff = new HashMap<>();
-//			ArrayList<String> fields = new ArrayList<>();
-//			for (int i = 0; i < fieldlist.size(); i++) {
-//				// fieldCnt += 1;
-//				String off = String.valueOf(fieldCnt * 4 + 4);
-//				offsets.put(id + "_" + (((fieldlist.get(i)).get(0)).get(0)), off);
-//				fieldCnt += 1;
-//				fields.add(((fieldlist.get(i)).get(0)).get(0));
-//			}
-//			for (int i = 0; i < funclist.size(); i++) {
-//				offsets.put(id + "_" + ((funclist.get(i)).get(0)), ((funclist.get(i)).get(1)) + "_" + ((funclist.get(i)).get(0)));
-//				funcOff.put(((funclist.get(i)).get(0)), i * 4);
-//			}
-//
-//			classFuncsOff.put(id, funcOff);
-//
-//			// System.out.println(fieldlist);
-//			// System.out.println(funclist);
-//			// #######################part 4 - mips function
-//			// ################################
-//			// son funcs
-//			for (AST_CFEILD_LIST it = data_members; it != null; it = it.tail) {
-//				if (it.head instanceof AST_CFEILD_FUNCDEC)
-//					it.head.IRme();
-//			}
-//			IR.getInstance().Add_IRcommand(new IRcommand_declareClass(id, funclist, fieldlist));
-//			// fields
-//			AST_CFEILD_LIST temp = data_members;
-//			for (int i = 0; i < fieldlist.size(); i++) {
-//				String tf = fieldlist.get(i).get(1).get(0);
-//				String n = fieldlist.get(i).get(0).get(0);
-//				if (!(tf.equals(id))) // father fields
-//				{
-//					String v = defaultFields.get(tf + "_" + n);
-//					if (v == null)
-//						IR.getInstance().Add_IRcommand(new IRcommand_Declare_Global_Object(id + "_" + n));
-//					else if (fieldlist.get(i).get(0).get(1).equals("1")) // string
-//						IR.getInstance().Add_IRcommand(new IRcommand_Declare_Global_String(id + "_" + n, v));
-//					else// int
-//						IR.getInstance().Add_IRcommand(new IRcommand_Declare_Global_Int(id + "_" + n, Integer.valueOf(v)));
-//				} else // son feilds
-//				{
-//					for (AST_CFEILD_LIST it = temp; it != null; it = it.tail) {
-//						if (it.head instanceof AST_CFEILD_VARDEC &&
-//								((AST_CFEILD_VARDEC) it.head).vd.id.equals(n)) {
-//							it.head.IRme();
-//							temp = data_members;
-//							break;
-//						}
-//					}
-//				}
-//			}
-//
-//			classSize.put(id, fieldlist.size() * 4 + 4);
-//			classfields.put(id, fields);
+		} else {
+			// Gal stopped - TODO
+			TYPE_CLASS fatherclass = (TYPE_CLASS) SYMBOL_TABLE.getInstance().find(id2);
+			boolean f = false;
+			// ############first part (taking father fields and funcs)#################
+			ArrayList<ArrayList<ArrayList<String>>> fieldlist = new ArrayList<ArrayList<ArrayList<String>>>();
+			ArrayList<ArrayList<String>> funclist = new ArrayList<ArrayList<String>>();
+			while (fatherclass != null) {
+				for (TYPE_LIST it = fatherclass.data_members; it != null; it = it.tail) {
+					if (it.typeName == "function") {
+						// add to funcList and check that it's only one time in the list
+						int n = funclist.size();
+						for (int i = 0; i < n; i++) {
+							f = false;
+							if (((funclist.get(i)).get(0)).equals(it.head.name)) {
+								ArrayList<String> temp = funclist.get(i);
+								funclist.remove(i);
+								funclist.add(0, temp);
+								break;
+							}
+						}
+						if (f == false) {
+							ArrayList<String> funcOfFather = new ArrayList<String>();
+							funcOfFather.add(it.head.name);
+							funcOfFather.add(fatherclass.name);
+							funclist.add(0, funcOfFather);
+						}
+					} else { // it's variable
+						f = false;
+						int n = fieldlist.size();
+						for (int i = 0; i < n; i++) {
+							if ((((fieldlist.get(i)).get(0)).get(0)).equals(it.head.id)) {
+								f = true;
+								ArrayList<ArrayList<String>> temp = fieldlist.get(i);
+								fieldlist.remove(i);
+								fieldlist.add(0, temp);
+								break;
+							}
+						}
+						if (f == true) // the element was twice
+							continue;
+
+						ArrayList<String> fieldOfFather = new ArrayList<String>();
+						fieldOfFather.add(it.head.id);
+						if (it.head.typeName == "string")
+							fieldOfFather.add("1");
+						else
+							fieldOfFather.add("0");
+						ArrayList<ArrayList<String>> field = new ArrayList<ArrayList<String>>();
+						field.add(fieldOfFather);
+						ArrayList<String> fatherarray = new ArrayList<String>();
+						fatherarray.add(fatherclass.name);
+						field.add(fatherarray);
+						fieldlist.add(0, field);
+					}
+				}
+				fatherclass = fatherclass.father;
+			}
+
+			// #############################################################3
+			// #################second part - this class fields and funcs
+			for (AST_C_FEILD_LIST it = cFieldList; it != null; it = it.tail) {
+				AST_C_FIELD field = (AST_C_FIELD) (it.head);
+				if (field.typeCField == "varDec") { // field
+					f = false;
+					AST_C_FIELD_VAR a = (AST_C_FIELD_VAR) field;
+					AST_VAR_DEC b = (AST_VAR_DEC) (a.varDec);
+					int n = fieldlist.size();
+					for (int i = 0; i < n; i++) {
+						if ((((fieldlist.get(i)).get(0)).get(0)).equals(b.id)) {
+							f = true;
+							((fieldlist.get(i)).get(1)).set(0, id);
+							break;
+						}
+					}
+					if (f == false) {
+						ArrayList<String> fieldOfMe = new ArrayList<String>();
+						fieldOfMe.add(b.id);
+						if (b.typeName == "string")
+							fieldOfMe.add("1");
+						else
+							fieldOfMe.add("0");
+						ArrayList<ArrayList<String>> field2 = new ArrayList<ArrayList<String>>();
+						field2.add(fieldOfMe);
+						ArrayList<String> mearray = new ArrayList<String>();
+						mearray.add(id);
+						field2.add(mearray);
+						fieldlist.add(field2);
+					}
+					continue;
+				}
+				if (field.typeCField == "funcDec") { // func
+					f = false;
+					AST_C_FIELD_FUNC a = (AST_C_FIELD_FUNC) field;
+					AST_FUNC_DEC b = (AST_FUNC_DEC) (a.funcDec);
+					int n = funclist.size();
+					for (int i = 0; i < n; i++) {
+						if (((funclist.get(i)).get(0)).equals(b.id)) {
+							f = true;
+							(funclist.get(i)).set(1, id);
+							break;
+						}
+					}
+					if (f == false) {
+						ArrayList<String> funcOfMe = new ArrayList<String>();
+						funcOfMe.add(b.id);
+						funcOfMe.add(id);
+						funclist.add(funcOfMe);
+					}
+
+				}
+			}
+
+			// #######################part 3 - offsets ################################
+			int funcCnt = funclist.size();
+			int fieldCnt = 0;
+			Map<String, Integer> funcOff = new HashMap<>();
+			ArrayList<String> fields = new ArrayList<>();
+			for (int i = 0; i < fieldlist.size(); i++) {
+				// fieldCnt += 1;
+				String off = String.valueOf(fieldCnt * 4 + 4);
+				offsets.put(id + "_" + (((fieldlist.get(i)).get(0)).get(0)), off);
+				fieldCnt += 1;
+				fields.add(((fieldlist.get(i)).get(0)).get(0));
+			}
+			for (int i = 0; i < funclist.size(); i++) {
+				offsets.put(id + "_" + ((funclist.get(i)).get(0)), ((funclist.get(i)).get(1)) + "_" + ((funclist.get(i)).get(0)));
+				funcOff.put(((funclist.get(i)).get(0)), i * 4);
+			}
+
+			classFuncsOff.put(id, funcOff);
+
+			// System.out.println(fieldlist);
+			// System.out.println(funclist);
+			// #######################part 4 - mips function
+			// ################################
+			// son funcs
+			for (AST_C_FEILD_LIST it = cFieldList; it != null; it = it.tail) {
+				if (it.head.typeCField == "funcDec")
+					it.head.IRme();
+			}
+			IR.getInstance().Add_IRcommand(new IRcommand_declareClass(id, funclist, fieldlist));
+			// fields
+			AST_C_FEILD_LIST temp = cFieldList;
+			for (int i = 0; i < fieldlist.size(); i++) {
+				String tf = fieldlist.get(i).get(1).get(0);
+				String n = fieldlist.get(i).get(0).get(0);
+				if (!(tf.equals(id))) // father fields
+				{
+					String v = defaultFields.get(tf + "_" + n);
+					if (v == null)
+						IR.getInstance().Add_IRcommand(new IRcommand_Declare_Global_Object(id + "_" + n));
+					else if (fieldlist.get(i).get(0).get(1).equals("1")) // string
+						IR.getInstance().Add_IRcommand(new IRcommand_Declare_Global_String(id + "_" + n, v));
+					else// int
+						IR.getInstance().Add_IRcommand(new IRcommand_Declare_Global_Int(id + "_" + n, Integer.valueOf(v)));
+				} else // son feilds
+				{
+					for (AST_C_FEILD_LIST it = temp; it != null; it = it.tail) {
+						if (it.head.typeCField == "varDec" &&
+								((AST_C_FIELD_VAR) it.head).varDec.id.equals(n)) {
+							it.head.IRme();
+							temp = cFieldList;
+							break;
+						}
+					}
+				}
+			}
+
+			classSize.put(id, fieldlist.size() * 4 + 4);
+			classfields.put(id, fields);
 		}
 	}
 }
