b402657c01e61c1b559a1ba4d5aa940894e83e86
royddf503 <royddf1@hotmail.com>
2023-03-16 01:03:08 +0200

    finish the logic of IRME

--- ex4/src/IR/IRcommand_Declare_Class.java
+++ ex4/src/IR/IRcommand_Declare_Class.java
@@ -21,6 +21,6 @@ public class IRcommand_Declare_Class extends IRcommand {
 
 	public void MIPSme() {
 		System.out.println("IRcommand_Declare_Class" + "- MIPSme");
-		MIPSGenerator.getInstance().declare_class(class_name, funcs, fs);
+		MIPSGenerator.getInstance().create_new_class(class_name, funcs, fs);
 	}
 }
